389664eca798164b6a92e5692f1d189435cec7c1
andrenguyening <andredbooneschool@gmail.com>
2024-03-18 17:27:13 -0400

    added images

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -15,14 +15,13 @@
 		  <li><a href="/"><i class="fa-solid fa-house" id = "house"></i><span class= "iconlabel">Home</span></a></li>
 		  <li><a href="/about"><i class="fa-solid fa-user"></i><span class= "iconlabel">About</span></a></li>
 		  <li><a href="/projects"><i class="fa-solid fa-code"></i><span class= "iconlabel">Projects</span></a></li>
-		  <li><a href="https://drive.google.com/file/d/1QmQlTtdfx9uxaN1aG5E8cTWa5n13Zf74/view?usp=sharing">Resume</a></li>
+		  <li><a href="https://drive.google.com/file/d/1QmQlTtdfx9uxaN1aG5E8cTWa5n13Zf74/view?usp=sharing"><i class="fa-solid fa-file"></i><span class= "iconlabel">Resume</span></a></li>
 		  <!-- <li><a href="/contact">Contact</a></li> -->
 		</ul>
 	</div>
   </nav>
 
   <style>
-    /* Your styling for the navbar goes here */
     #name{
         color:rgb(89, 128, 255);
     }
