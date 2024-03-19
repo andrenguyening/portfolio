cca8d0d6cd8a276b2c34925b5691855a28440bf4
andrenguyening <andredbooneschool@gmail.com>
2024-03-19 01:29:57 -0400

    changed resume path

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    import pdf from "../../src/public/pdf/AndreNguyen_Spring2024.pdf"
+    import pdf from "../../public/pdf/AndreNguyen_Spring2024.pdf"
 </script>
 
 <iframe src={pdf} title="PDF Viewer" width="100%" height="500px"></iframe>
